1ce70e9b8adcb48b99f396dfd7b06c33ebee7892
Olivier9493 <oli4@pachyderme.li>
2018-03-18 18:21:45 +0100

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Oli4Becker on 18.03.18.
+//  Copyright © 2018 Oli4Becker. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
